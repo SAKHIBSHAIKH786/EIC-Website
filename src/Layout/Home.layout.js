89a84eda7113c257a2fa4eee97a5dc50fc39b572
Janak Khorgade <janak24fop@gmail.com>
2021-11-02 18:16:04 +0530

    Team Section almost done

--- src/Layout/Home.layout.js
+++ src/Layout/Home.layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MyFaqSection from "../Components/FAQ/MyFaqAccordion";
 import HeroCarousal from "../Components/Carousal/HeroCarousal";
+import TeamContainer from "../Components/team/team";
 
 const HomeLayout = (props) => {
   return (
     <>
       <HeroCarousal />
       {props.children}
+      <TeamContainer />
       <MyFaqSection />
     </>
   );
