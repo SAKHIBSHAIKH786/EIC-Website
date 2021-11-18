3cf69a048739f4f3da46ade9f392f670cc30f8bb
Shubham Jadhav <56510414+ShubhamSj07@users.noreply.github.com>
2021-11-19 00:11:43 +0530

    Update faqData.js

--- src/Components/FAQ/faqData.js
+++ src/Components/FAQ/faqData.js
@@ -1,22 +1,22 @@
 export const faqs = [
   {
     id: 1,
-    question: "What is Electro Infinity Club about?",
-    answer: "It is a Student's Club formed under E&TC Department of Modern Education Society's College of Engineering,Pune.",
+    question: "What is Electro Infinity Club ?",
+    answer: "It is a Student's Club formed under E&TC Department of Modern Education Society's College of Engineering, Pune, where students carry out diverse activities.",
   },
   {
     id: 2,
-    question: "Why was the club established?",
-    answer: "The main idea behind the formation of the club is to help and nurture students who want to learn and build their careers in Electronics.",
+    question: "Can I Join? How ?",
+    answer: "If you're reading the answer to this question, trust me you'll enjoy each of our events.Joining EIC is very simple, just click the `join the club` button above, fillout the form.Becoming a member of any club has never been so easy, has it ?",
   },
   {
     id: 3,
-    question: "What are the activities held by the Electro Infinity Club?",
-    answer: "The activities of the club are based on the electronic domains to make students engage and interested in that field. ",
+    question: "What are the activities held by the Electro Infinity Club ?",
+    answer: "The activities of the club are based on the electronic domains to make students engage, build connections, interact with peers, explore new opportunities.",
   },
   {
     id: 4,
-    question: "Are the club activities open to student from all branches?",
-    answer: "Yes, We invite students from all branches interested in learning new things to attend our events and acquire knowledge from them. ",
+    question: "Are there any fees charged ?",
+    answer: "Do you think we can charge you for this? This is 21st century where you don't need to pay anything for knowledge.So, don't worry, we won't charge you for any of our events",
   }
 ];
