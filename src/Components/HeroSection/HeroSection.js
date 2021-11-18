1400aec280ab31bb7a2ea56d3ada7fca483381b7
Janak Khorgade <janak24fop@gmail.com>
2021-11-18 10:56:48 +0530

    did some code cleaning

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const HeroSection = () => {
-	return (
-		<>
-		<header className="hero">
-			{/*<h1 className="heroText hidden">Electro Infinity Club</h1>*/}
-			{/*<h3 className="heroSubText hidden">Official Student's Club of Modern Education Society's College of Engineering</h3>*/}
-		</header>
-		</>
-	);
+  return (
+    <>
+    <header className="hero">
+    </header>
+    </>
+  );
 };
 
 export default HeroSection;
--- src/Components/team/faculty.js
+++ src/Components/team/faculty.js
@@ -2,55 +2,55 @@ import React from "react";
 import "./faculty.css";
 
 const Faculty = () => {
-	return (
-		<>
-			<section className="container mb-12 mx-auto">
-				<div className="faculty-container">
-					<div className="faculty-card">
-						<div className="faculty-image-container">
-							<img
-								className="faculty-image"
-								src="https://github.com/Electro-Infinity-Club/Team-Member-Images/blob/master/faculty/Prof-KS-Tiwari.jpg?raw=true"
-								alt="professor"
-							/>
-						</div>
-						<div className="faculty-content">
-							<h1 className="faculty-name text-club-900">
-								Dr. K.S.Tiwari
-							</h1>
-							<p className="text-base font-normal text-club-700">
-								Dr K.S.Tiwari completed her graduation in 1999 from RKN. 
-								She completed her M.Tech and PhD from VNIT Nagpur and has more than 20 years of teaching experience. 
-								She is a life member of several reputed bodies like ISTE, IETE, IAENG, RST and has credit to publications in several reputed journals. 
-								Her area of expertise is VLSI, Digital Design and Rough set theory.
-							</p>
-						</div>
-					</div>
+  return (
+    <>
+      <section className="container mb-12 mx-auto">
+        <div className="faculty-container">
+          <div className="faculty-card">
+            <div className="faculty-image-container">
+              <img
+                className="faculty-image"
+                src="https://github.com/Electro-Infinity-Club/Team-Member-Images/blob/master/faculty/Prof-KS-Tiwari.jpg?raw=true"
+                alt="professor"
+              />
+            </div>
+            <div className="faculty-content">
+              <h1 className="faculty-name text-club-900">
+                Dr. K.S.Tiwari
+              </h1>
+              <p className="text-base font-normal text-club-700">
+                Dr K.S.Tiwari completed her graduation in 1999 from RKN. 
+                She completed her M.Tech and PhD from VNIT Nagpur and has more than 20 years of teaching experience. 
+                She is a life member of several reputed bodies like ISTE, IETE, IAENG, RST and has credit to publications in several reputed journals. 
+                Her area of expertise is VLSI, Digital Design and Rough set theory.
+              </p>
+            </div>
+          </div>
 
-					<div className="faculty-card">
-						<div className="faculty-image-container">
-							<img
-								className="faculty-image"
-								src="https://github.com/Electro-Infinity-Club/Team-Member-Images/blob/master/faculty/Prof-Yogita-Ajgar.jpg?raw=true"
-								alt="professor"
-							/>
-						</div>
-						<div className="faculty-content">
-							<h1 className="faculty-name text-club-900">
-								Prof. Yogita Ajgar
-							</h1>
-							<p className="text-base font-normal text-club-700 md:py-3 lg:py-0">
-								Prof Yogita Ajgar completed her graduation in 2001 from Pune University and M.E. from PICT college, Pune. 
-								She is pursuing her PhD in the field of Machine learning and has more than 15 years of teaching experience. 
-								She is a life member of several reputed bodies like ISTE, IETE.
-								Her area of expertise is Microwave and Communication.
-							</p>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+          <div className="faculty-card">
+            <div className="faculty-image-container">
+              <img
+                className="faculty-image"
+                src="https://github.com/Electro-Infinity-Club/Team-Member-Images/blob/master/faculty/Prof-Yogita-Ajgar.jpg?raw=true"
+                alt="professor"
+              />
+            </div>
+            <div className="faculty-content">
+              <h1 className="faculty-name text-club-900">
+                Prof. Yogita Ajgar
+              </h1>
+              <p className="text-base font-normal text-club-700 md:py-3 lg:py-0">
+                Prof Yogita Ajgar completed her graduation in 2001 from Pune University and M.E. from PICT college, Pune. 
+                She is pursuing her PhD in the field of Machine learning and has more than 15 years of teaching experience. 
+                She is a life member of several reputed bodies like ISTE, IETE.
+                Her area of expertise is Microwave and Communication.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Faculty;
